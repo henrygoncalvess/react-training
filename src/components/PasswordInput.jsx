930dd3ca1f2c310212a4b0527cdd7717162db1bd
henrygoncalvess <octanebt@gmail.com>
2025-07-05 19:27:34 -0300

    feat: add `reset.css` style and create `PasswordInput` component

---
+++ src/components/PasswordInput.jsx
@@ -0,0 +1,17 @@
+// import { useState } from "react";
+
+export default function PasswordInput() {
+  return (
+    <>
+      <input
+        className="inputPass"
+        type="text"
+        id="pass"
+        placeholder="your password"
+        value="123"
+      />
+
+      <div className="passwordAlert">teste</div>
+    </>
+  );
+}
