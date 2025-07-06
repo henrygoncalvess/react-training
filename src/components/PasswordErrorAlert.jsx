abfe1b21d7fcf223da85e90b11996ccb39263460
henrygoncalvess <octanebt@gmail.com>
2025-07-06 04:29:37 -0300

    fix: fixes missing `&bull;` and adds new checker

--- src/components/PasswordErrorAlert.jsx
+++ src/components/PasswordErrorAlert.jsx
@@ -11,12 +11,17 @@ export default function PasswordErrorAlert({ password }) {
         <b style={{ fontSize: "1.1em" }}>{"!@#$%^&*.()"}</b>
       </p>
       <p className={/[A-Z]/.test(password) ? valid : invalid}>
-        1 Letra maiúscula A-Z
+        &bull; 1 Letra maiúscula A-Z
       </p>
       <p className={/[a-z]/.test(password) ? valid : invalid}>
-        1 Letra minúscula a-z
+        &bull; 1 Letra minúscula a-z
       </p>
-      <p className={/[0-9]/.test(password) ? valid : invalid}>1 Número 0-9</p>
+      <p className={/[0-9]/.test(password) ? valid : invalid}>
+        &bull; 1 Número 0-9
+      </p>
+      <p className={password.length >= 8 ? valid : invalid}>
+        &bull; 8 caracteres
+      </p>
     </>
   );
 }
